562f0047350918d91abb9d2c5c81eb7950d95b4f
Lakshan669 <125352205+Lakshan669@users.noreply.github.com>
2023-02-14 15:29:08 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,18 +12,18 @@ global.APIKeys = {
 }
 
 //customize settings\\
-global.owner = ['94784506970']
-global.premium = ['94784506970']
-global.ownernomer = '94784506970'
-global.ownername = 'Achi Fernando'
-global.botname = '🎭ᴀʟᴘʜᴀ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ🎭 v1.0.0'
-global.footer = ' ᴀʟᴘʜᴀ ʙᴏᴛ-ᴍᴅ ᴇᴅɪᴛɪᴏɴ'
-global.ig = 'https://github.com/ALPHA-OFFICIAL-TEAM'
+global.owner = ['94766370881']
+global.premium = ['94766370881']
+global.ownernomer = '94766370881'
+global.ownername = 'Lakshan'
+global.botname = '🎭Lakiya WA BOT🎭 '
+global.footer = ' Lakiya WA BOT'
+global.ig = 'https://github.com/Lakshan669/ALPHA-MD-WA-BOT'
 global.region = 'India, South India, Kerala'
-global.sc = 'https://github.com/ALPHA-OFFICIAL-TEAM'
-global.myweb = 'https://www.youtube.com/c/AchiBrolk'
-global.packname = 'ALPHA WA BOT MD'
-global.author = 'D.U.A.N.FERNANDO '
+global.sc = 'https://github.com/Lakshan669/ALPHA-MD-WA-BOT'
+global.myweb = '-'
+global.packname = 'Lakiya WA BOT'
+global.author = ' Lakshan'
 global.sessionName = 'session'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '🚢'
